08fd5b6e341a937694a5c75a6ec5534ce6f8bb2e
Danialddm <dd.iebs@gmail.com>
2022-07-22 15:55:33 +0430

    dd adds the first version of web api of fetching itemid and qty

--- PythonWebApi.py
+++ PythonWebApi.py
@@ -5,28 +5,32 @@ from flask_jsonpify import jsonify
 import pyodbc
 
 # DB connection
-server = '10.192.63.52\DWH01,49149' 
+server = '172.31.3.245'#'10.192.63.52\DWH01,49149' 
 print(server)
-database = 'WebSiteDB' 
-username = 'websiteuser' 
-password = 'B$tor$4416#Bos' 
-cnxn = pyodbc.connect("DRIVER={ODBC Driver 17 for SQL Server};"
-                        "SERVER= GIG001VERP02;"#10.192.63.52\DWH01,49149;"
-                        "DATABASE= OKDynAXTest;"#WebSiteDB;"
-                        #"UID= websiteuser;"
-                        #"PWD= B$tor$4416#Bos;"
-                        "Trusted_Connection=yes;")
+database = 'OKDynAXTest'#'WebSiteDB' 
+username = 'lotteryuser'#'websiteuser' 
+password = '123'#'B$tor$4416#Bos' 
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-conn = pyodbc.connect()
+query = cursor.execute("select top 10 itemid,qty from retailtransactionsalestrans;")
+columns = [column[0] for column in cursor.description]
+results = []
+for row in cursor.fetchall():
+    results.append(dict(zip(columns, row)))
+
+#result = {'data': [dict(zip(tuple (query) ,i)) for i in cursor]}
+
+
+
 
 app = Flask(__name__)
 api = Api(app)
 
-class Ids(Resource):
+class ItemID(Resource):
     def get(self):
-        return {'ids': [1,2,3]}
+        return jsonify(results)
 
-api.add_resource(Ids, '/ids') 
+api.add_resource(ItemID, '/itemid') 
 
 if __name__ == '__main__':
      app.run(port='5002')
