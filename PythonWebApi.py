8112472d18fb89c23be51b53bdf67231737ad7db
Danialddm <dd.iebs@gmail.com>
2022-07-22 20:32:32 +0430

    ddm adds the another approach to show data in api with parameters

--- PythonWebApi.py
+++ PythonWebApi.py
@@ -6,31 +6,27 @@ import pyodbc
 
 # DB connection
 server = '172.31.3.245'#'10.192.63.52\DWH01,49149' 
-print(server)
 database = 'OKDynAXTest'#'WebSiteDB' 
 username = 'lotteryuser'#'websiteuser' 
 password = '123'#'B$tor$4416#Bos' 
 cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
 cursor = cnxn.cursor()
-query = cursor.execute("select top 10 itemid,qty from retailtransactionsalestrans;")
-columns = [column[0] for column in cursor.description]
 results = []
-for row in cursor.fetchall():
-    results.append(dict(zip(columns, row)))
-
-#result = {'data': [dict(zip(tuple (query) ,i)) for i in cursor]}
-
-
-
 
 app = Flask(__name__)
 api = Api(app)
 
-class ItemID(Resource):
-    def get(self):
-        return jsonify(results)
+@app.route('/get')
+#class ItemID(Resource):
+def get():
+    itemid = request.args.get('itemid')
+    query = cursor.execute("select itemid,qty from retailtransactionsalestrans where itemid=" + itemid)
+    columns = [column[0] for column in cursor.description]
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
+    return jsonify(results)
 
-api.add_resource(ItemID, '/itemid') 
+#api.add_resource(ItemID, '/itemid') 
 
 if __name__ == '__main__':
      app.run(port='5002')
