1b9154d02b48b3c078165b15bea878a9995ca72a
Danialddm <dd.iebs@gmail.com>
2022-07-23 08:49:10 +0430

    ddm adds the list to get_itemid method

--- PythonWebApi.py
+++ PythonWebApi.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful  import Resource, Api
+from flask_restful  import Resource, Api, reqparse
 from json import dumps
 from flask_jsonpify import jsonify
 import pyodbc
@@ -16,16 +16,42 @@ results = []
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/get')
-#class ItemID(Resource):
-def get():
-    itemid = request.args.get('itemid')
-    query = cursor.execute("select itemid,qty from retailtransactionsalestrans where itemid=" + itemid)
+
+@app.route('/get_itemid', methods = ['GET'])
+def get_itemid():
+    itemid = request.args.getlist('itemid', type = str);
+    for x in itemid:        
+        query = cursor.execute("select itemid,AvailPhysical,storeid from [dbo].[OnHandItemsStock] where itemid=" + x)
+        columns = [column[0] for column in cursor.description]
+        for row in cursor.fetchall():
+            results.append(dict(zip(columns, row)))
+    return jsonify(results)
+
+@app.route('/get_batch', methods = ['GET'])
+def get_batch():
+    num = int(request.args.get('num'))
+    query = cursor.execute("select top " + str(num) + "itemid,AvailPhysical,storeid from [dbo].[OnHandItemsStock] order by itemid desc")
     columns = [column[0] for column in cursor.description]
     for row in cursor.fetchall():
         results.append(dict(zip(columns, row)))
     return jsonify(results)
 
+@app.route('/get_storeid', methods = ['GET'])
+def get_storeid():
+    storeid = request.args.get('storeid')
+    query = cursor.execute("select itemid,AvailPhysical,storeid from [dbo].[OnHandItemsStock] where storeid=" + storeid)
+    columns = [column[0] for column in cursor.description]
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
+    return jsonify(results)
+
+@app.route('/get_Allitemid', methods = ['GET'])
+def get_Allitemid():
+    query = cursor.execute("select itemid,AvailPhysical,storeid from [dbo].[OnHandItemsStock] order by itemid desc")
+    columns = [column[0] for column in cursor.description]
+    for row in cursor.fetchall():
+        results.append(dict(zip(columns, row)))
+    return jsonify(results)
 #api.add_resource(ItemID, '/itemid') 
 
 if __name__ == '__main__':
